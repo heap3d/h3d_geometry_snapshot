745b6b0c5b405f49298f999f266ab103f9d51ace
Dmytro Holub <heap3d@gmail.com>
2024-08-21 19:01:41 +0300

    initial commit

---
+++ scripts/replicator_individual_snapshot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# ================================
+# (C)2024 Dmytro Holub
+# heap3d@gmail.com
+# --------------------------------
+# modo python
+# EMAG
+# make individual replicator for each selected items snapshot
+# ================================
+
+
+import modo
+import lx
+
+
+def main():
+    ...
+
+
+if __name__ == '__main__':
+    main()
