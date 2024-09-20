bcd25bb4a08bd3a6605d53ce26ff14ee52248685
Dmytro Holub <heap3d@gmail.com>
2024-09-20 12:10:07 +0300

    added: hierarchy support in copies

--- scripts/merge_meshes_individual_snapshot.py
+++ scripts/merge_meshes_individual_snapshot.py
@@ -13,7 +13,7 @@ import modo
 import lx
 import modo.constants as c
 
-from h3d_utilites.scripts.h3d_utils import itype_str
+from h3d_utilites.scripts.h3d_utils import itype_str, parent_items_to
 from h3d_utilites.scripts.h3d_debug import H3dDebug
 
 from h3d_geometry_snapshot.scripts.merge_meshes_snapshot import (
@@ -55,38 +55,52 @@ def main():
     preset_browser_opened = open_preset_browser()
     new_items: list[modo.Item] = []
 
+    # dict(original: copy)
+    copies: dict[modo.Item, modo.Item] = dict()
+
     for nonreplicator in nonreplicators:
         add_to_schematic((nonreplicator,), workspace)
-        merged_nonreplicators = modo.Scene().addMesh(rename(nonreplicator.name))
-        add_to_schematic((merged_nonreplicators,), workspace)
-        merged_nonreplicators.select(replace=True)
+        merged_nonreplicator = modo.Scene().addMesh(rename(nonreplicator.name))
+        add_to_schematic((merged_nonreplicator,), workspace)
+        merged_nonreplicator.select(replace=True)
 
         lx.eval('select.filepath "[itemtypes]:MeshOperations/edit/pmodel.meshmerge.itemtype" set')
         lx.eval('select.preset "[itemtypes]:MeshOperations/edit/pmodel.meshmerge.itemtype" mode:set')
         lx.eval('preset.do')
 
-        merge_meshes_meshop_nonreplicators = modo.Scene().selectedByType(itype='pmodel.meshmerge')[0]
+        merge_meshes_meshop_nonreplicator = modo.Scene().selectedByType(itype='pmodel.meshmerge')[0]
         lx.eval('item.channel pmodel.meshmerge$copyNormal true')
-        link_to_merge_meshes((nonreplicator,), merge_meshes_meshop_nonreplicators)
-        new_items.append(merged_nonreplicators)
+        link_to_merge_meshes((nonreplicator,), merge_meshes_meshop_nonreplicator)
+        new_items.append(merged_nonreplicator)
+
+        copies[nonreplicator] = merged_nonreplicator
 
     for replicator in replicators:
         add_to_schematic(replicators, workspace)
-        merged_replicators = modo.Scene().addMesh(rename(replicator.name))
-        add_to_schematic((merged_replicators,), workspace)
-        merged_replicators.select(replace=True)
+        merged_replicator = modo.Scene().addMesh(rename(replicator.name))
+        add_to_schematic((merged_replicator,), workspace)
+        merged_replicator.select(replace=True)
 
         lx.eval('select.filepath "[itemtypes]:MeshOperations/edit/pmodel.meshmerge.itemtype" set')
         lx.eval('select.preset "[itemtypes]:MeshOperations/edit/pmodel.meshmerge.itemtype" mode:set')
         lx.eval('preset.do')
 
-        merge_meshes_meshop_replicators = modo.Scene().selectedByType(itype='pmodel.meshmerge')[0]
+        merge_meshes_meshop_replicator = modo.Scene().selectedByType(itype='pmodel.meshmerge')[0]
         lx.eval('item.channel pmodel.meshmerge$copyNormal true')
         lx.eval('item.channel pmodel.meshmerge$world false')
-        link_to_merge_meshes((replicator,), merge_meshes_meshop_replicators)
-        new_items.append(merged_replicators)
+        link_to_merge_meshes((replicator,), merge_meshes_meshop_replicator)
+        new_items.append(merged_replicator)
+
+        copies[replicator] = merged_replicator
 
     restore_preset_browser(preset_browser_opened)
+
+    for item in nonreplicators:
+        parent_items_to([copies[item],], copies.get(item.parent, None))  # type: ignore
+
+    for item in replicators:
+        parent_items_to([copies[item],], copies.get(item.parent, None))  # type: ignore
+
     modo.Scene().deselect()
     for item in new_items:
         item.select()
